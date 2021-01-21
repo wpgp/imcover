a481e6b1ee897b2c6c1ee5c7ed2303fb0a3665f4
Chris Jochem <wcjochem@users.noreply.github.com>
2021-01-21 11:04:17 +0000

    update subsetting

--- R/ic.R
+++ R/ic.R
@@ -201,7 +201,8 @@ is.ic_data <- function(object){
 #' @name ic.df
 #' @export
 "[.ic.df" <- function(x, i, j, ..., drop = FALSE){
-  nargs <- nargs()
+  mdrop <- missing(drop)
+  nargs <- nargs() - !mdrop
   attrs <- attributes(x)
   cattrs <- attrs[names(attrs) %in% ic_core()]
 
@@ -264,8 +265,41 @@ is.ic_data <- function(object){
 
 
 #' @export
-"$<-.ic.df" <- function(x, i, value){
-  x[[i]] <- value
+"$<-.ic.df" <- function(x, name, value){
+  attrs <- get_attr(x, ic_core(), unlist = TRUE)
+
+  if(!(name %in% attrs && is.null(value))){
+    # x[[name]] <- value
+  } else{
+    warning("Cannot drop core 'ic' data columns.", call. = FALSE, )
+  }
+
+  # x[[name]] <- value
+
+  return(x)
+  # x[[i]] <- value
+  # cl <- oldClass(x)
+  # class(x) <- NULL
+  # nrows <- .row_names_info(x, 2L)
+  # if (!is.null(value)) {
+  #   N <- NROW(value)
+  #   if (N > nrows)
+  #     stop(sprintf(ngettext(N, "replacement has %d row, data has %d",
+  #                           "replacement has %d rows, data has %d"), N, nrows),
+  #          domain = NA)
+  #   if (N < nrows)
+  #     if (N > 0L && (nrows%%N == 0L) && length(dim(value)) <=
+  #         1L)
+  #       value <- rep(value, length.out = nrows)
+  #     else stop(sprintf(ngettext(N, "replacement has %d row, data has %d",
+  #                                "replacement has %d rows, data has %d"), N, nrows),
+  #               domain = NA)
+  #     if (is.atomic(value) && !is.null(names(value)))
+  #       names(value) <- NULL
+  # }
+  # x[[name]] <- value
+  # class(x) <- cl
+  # return(x)
 }
 
 
@@ -411,12 +445,12 @@ list_vaccines <- function(X){
 
 
 #' List core attributes for ic data
-#' Retrieve the names of the core attributes that should be defined in
+#' Retrieve the names of the core attributes that can be expected in
 #' \code{ic.df} data.
 #' @name ic_core
 #' @export
 ic_core <- function(){
-  return(c("group","time","vaccine","coverage","dose","population"))
+  return(c("group", "time", "vaccine", "coverage", "dose", "population"))
 }
 
 
