d90dc19ed1a87e0dcde12b148f33cca70d4fb41a
Chris Jochem <wcjochem@users.noreply.github.com>
2021-01-26 15:20:47 +0000

    placeholder docs

---
+++ R/imcover.R
@@ -0,0 +1,11 @@
+#' imcover: An R package for spatio-temporal immunisation coverage modelling
+#'
+#' [Add some description here.]
+#'
+#' @section Credits:
+#' This work was undertaken by members of the WorldPop Research Group at the
+#' University of Southampton(\url{https://www.worldpop.org/}).
+#'
+#' @docType package
+#' @name imcover
+NULL
