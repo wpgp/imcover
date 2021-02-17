73c1436e924f19578d4c58dfb1ffdb90ba04e9c1
Chris Jochem <wcjochem@users.noreply.github.com>
2021-02-17 18:33:44 +0000

    get yovi data

--- R/utils.R
+++ R/utils.R
@@ -48,3 +48,66 @@ get_attr <- function(X, attrs = ic_core(), unlist = TRUE){
     return(attributes(X)[which(names(attributes(X)) %in% attrs)])
   }
 }
+
+
+# ic_filter <- function(X, yovi = 'default'){
+#   if(!is.ic_data(X)){
+#     stop("Please provide valid 'ic' data.")
+#   }
+#
+#   if(missing(yovi)){
+#     stop("Please provide a data.frame of vaccines, or select 'default'.")
+#   }
+#
+#   if(is.character(yovi)){
+#     if(yovi == "default"){
+#       yovi <- get_yovi()
+#     }
+#   } else{
+#     stopifnot(inherits(yovi, "data.frame"))
+#   }
+#
+#   # filter
+#   return(X)
+# }
+
+#' Get year of introduction for vaccines
+#' Retrieve a list of the year when a vaccine was introduced to the entire
+#' country.
+#' @param vaccine Vector of vaccine codes as characters. Default is \code{NULL}
+#'   which returns all vaccines.
+#' @details Data source of vaccine schedules:
+#'   \link{https://www.who.int/immunization/monitoring_surveillance/data/year_vaccine_introduction.xlsx}
+#'
+#' @name get_yovi
+#' @export
+get_yovi <- function(vaccine = NULL){
+  if(!is.null(vaccine)){
+    return(imcover:::yovi[vaccine %in% yovi$vaccine, ])
+  } else{
+    return(imcover:::yovi)
+  }
+}
+
+
+# add_region <- function(X, group, region = "who"){
+#   if(!is.ic_data(X) && missing(group)){
+#     stop("Please supply a grouping variable or 'ic' data.")
+#   }
+#
+#   if(missing(groupVar)){
+#     group <- attr(X, "group")
+#   } else if(!is.character(group)){
+#     stop("Please supply grouping variable as a character name.")
+#   }
+#
+#   if(length(group) > 1){ group <- group[1L] }
+#   if(!group %in% names(X)){ stop("Please supply a valid column name.")}
+#
+#   X[["region"]] <- lookup_region(X[[group]], region)
+# }
+
+
+# lookup_region <- function(code, region){
+#   return(region_tbl[region_tbl$code == code,][[region]])
+# }
