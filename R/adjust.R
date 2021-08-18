aa682ef26c399287f9ef283cde054431b31bdc3a
Chris Jochem <wcjochem@users.noreply.github.com>
2021-08-18 10:53:41 +0100

    calculate multi-dose ratios

--- R/adjust.R
+++ R/adjust.R
@@ -5,7 +5,7 @@
 #' preserve the ratio of multi-dose vaccines and that coverage is >0% and <100%.
 #' @param X Object of class \code{ic.df} to adjust
 #' @param ratio_adj Logical. Should multi-dose vaccine coverages be adjusted by
-#'   their ratio when coverage > 100%? Default is \code{TRUE}.
+#'   their ratio when coverage > 100%? Default is \code{FALSE}.
 #' @param numerator Character vector of vaccine identifiers (with dose numbers)
 #'   to calculate the ratio adjustment. Default is 'DTP3'.
 #' @param denominator Character vector of vaccine identifiers (with dose
@@ -23,7 +23,7 @@
 #' @seealso \code{\link[imcover]{ic_data}}
 #' @name ic_adjust
 #' @export
-ic_adjust <- function(X, ratio_adj = TRUE,
+ic_adjust <- function(X, ratio_adj = FALSE,
                       numerator = 'DTP3', denominator = 'DTP1',
                       coverage_adj = TRUE){
   if(missing(X) || !inherits(X, 'ic.df')){
@@ -46,7 +46,7 @@ ic_adjust <- function(X, ratio_adj = TRUE,
       X <- X[X[[attr(X, 'vaccine')]] != numerator[[i]], ]
 
       df <- merge(num,
-                  den[, get_attr(X, c('country', 'time', 'source', 'coverage')), drop = T],
+                  den[, get_attr(X, c('country', 'time', 'source', 'coverage'))],
                   by = get_attr(X, c('country', 'time', 'source')))
 
       df$above100 <- ifelse(df[[paste0(attr(X, 'coverage'), '.y')]] > 100,
@@ -91,5 +91,59 @@ ic_adjust <- function(X, ratio_adj = TRUE,
                                        X[[attr(X, 'coverage')]])
   }
 
+  return(X)
+}
+
+
+#' Get the ratio between multi-dose vaccines
+#'
+#' @param X Object of class \code{ic.df} to adjust
+#' @param numerator Character vector of vaccine identifiers (with dose numbers)
+#'   to calculate the ratio adjustment. Default is 'DTP3'.
+#' @param denominator Character vector of vaccine identifiers (with dose
+#'   numbers) to calculate the ratio adjustment. Default is 'DTP1'.
+#' @name ic_adjust
+#' @export
+ic_ratio <- function(X, numerator = 'DTP3', denominator = 'DTP1'){
+
+  if(missing(X) || !inherits(X, 'ic.df')){
+    stop('Please provide valid `ic` data')
+  }
+
+  if(any(!(c(numerator, denominator) %in% list_vaccines(X)))){
+    stop('Vaccines to adjust not found in ic data.')
+  }
+
+  if(length(numerator) != length(denominator)){
+    stop('Length of numerator and denominator for ratio adjustment do not match.')
+  }
+
+  for(i in 1:length(numerator)){
+    num <- as.data.frame(X[X[[attr(X, 'vaccine')]] == numerator[[i]], ])
+    den <- as.data.frame(X[X[[attr(X, 'vaccine')]] == denominator[[i]], ])
+    # drop vacc records
+    X <- X[X[[attr(X, 'vaccine')]] != numerator[[i]], ]
+
+    df <- merge(num,
+                den[, get_attr(X, c('country', 'time', 'source', 'coverage'))],
+                by = get_attr(X, c('country', 'time', 'source')))
+
+    # calculate the ratio - multiply by 100 to match coverage percentages
+    df$d_ratio <- df[[paste0(get_attr(X, 'coverage'), '.x')]] / df[[paste0(get_attr(X, 'coverage'), '.y')]] * 100
+
+    df$d_ratio <- ifelse(is.na(df$d_ratio) | df$d_ratio < 1, 0.1, df$d_ratio)
+    df$d_ratio <- ifelse(df$d_ratio >= 1, 99.9, df$d_ratio)
+    df$coverage <- df$d_ratio
+
+    # reassemble dataset
+    vd3 <- df[, c(intersect(names(df), names(X)))]
+    # convert to ic
+    vd3 <- do.call("ic_data", c(list(X=vd3), get_attr(X, ic_core())))
+
+    X <- rbind(X, vd3)
+  }
+
+  # add non-core attribute to flag data have been modified
+  attr(X, 'ratio') <- numerator
   return(X)
 }
