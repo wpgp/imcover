c1be2c422afbd0fbc44db334d9a3355b3439724a
Chris Jochem <wcjochem@users.noreply.github.com>
2021-08-18 09:25:25 +0100

    update docs

--- R/imcover.R
+++ R/imcover.R
@@ -6,13 +6,11 @@
 #' This work was undertaken by members of the WorldPop Research Group at the
 #' University of Southampton(\url{https://www.worldpop.org/}).
 #'
-#'
+#' @import Rcpp
+#' @import methods
+#' @importFrom rstan sampling
+#' @useDynLib imcover .registration = TRUE
 #'
 #' @docType package
 #' @name imcover
 NULL
-
-import(Rcpp)
-import(methods)
-importFrom(rstan, sampling)
-useDynLib(imcover, .registration = TRUE)
