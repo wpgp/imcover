87a16b773749032d068c41f390d8faa219215f1a
Chris Jochem <wcjochem@users.noreply.github.com>
2021-09-03 16:08:08 +0100

    update filter yovi

--- R/filter.R
+++ R/filter.R
@@ -103,3 +103,121 @@ ic_filter.iclist <- function(X, region, country, time, vaccine){
   return(X)
 }
 
+
+#' Filter ic data records based on year of vaccine introduction
+#' Exclude ic data records for years when a vaccine had not been fully
+#' introduced in a country.
+#' @param X An object of class \code{ic.df} or \code{icfit}.
+#' @param vaccine Vector of vaccine codes as characters. Default is \code{NULL}
+#'   which includes all vaccines in object \code{X}.
+#' @param yovi Table of the year of vaccine introduction. See details. Default
+#'   value comes from \code{imcover::get_yovi()}.
+#' @param na.rm Should records which do not have any data for year of
+#'   introduction be removed? Default is \code{FALSE} to keep \code{NA} records
+#'   in \code{X}.
+#' @return An object with type of \code{X} with records filtered by the year of
+#'   vaccine introductions.
+#' @details The \code{yovi} argument can take a user-defined table of values.
+#'   This must be a \code{data.frame} object with column names 'ISO3code',
+#'   'vaccine' and 'year_introduced'.
+#' @seealso \code{\link[imcover]{get_yovi}}
+#' @name filter_yovi
+#' @export
+filter_yovi <- function(X,
+                        vaccine = NULL,
+                        yovi = imcover::get_yovi(),
+                        na.rm = FALSE){
+  UseMethod("filter_yovi")
+}
+
+
+#' @name filter_yovi
+#' @export
+filter_yovi.ic.df <- function(X,
+                              vaccine = NULL,
+                              yovi = imcover::get_yovi(),
+                              na.rm = FALSE){
+
+  if(!inherits(yovi, "data.frame")){
+    stop("Please provide valid 'yovi' data.")
+  }
+
+  if(any(!c('ISO3Code', 'vaccine', 'year_introduced') %in% names(yovi))){
+    stop("Non-matching column names in YOVI table.")
+  }
+
+  if(!is.null(vaccine)){
+    yovi <- yovi[yovi$vaccine %in% vaccine, ]
+  } else{
+    vaccine <- unique(yovi$vaccine)
+  }
+  if(nrow(yovi) < 1L) stop("No vaccine records found in YOVI table.")
+
+  X <- merge(X, yovi[, c("ISO3Code", "vaccine", "year_introduced")],
+             by.x = get_attr(X, attrs = c("country", "vaccine")),
+             by.y = c("ISO3Code", "vaccine"),
+             suffixes = c("", ".yovi"),
+             all.x = TRUE, sort = FALSE)
+  nm <- c("year_introduced.yovi", "year_introduced")
+  nm <- nm[which(nm %in% names(X))]
+
+  # filter
+  if(na.rm){
+    del <- which((is.na(X[[nm]]) & X[[get_attr(X, "vaccine")]] %in% vaccine) | X[[get_attr(X, "time")]] <= X[[nm]])
+  } else{
+    del <- which(!is.na(X[[nm]]) & X[[get_attr(X, "time")]] <= X[[nm]])
+  }
+  X[[nm]] <- NULL
+
+  return(X[-del, ])
+}
+
+
+#' @name filter_yovi
+#' @export
+filter_yovi.icfit <- function(X,
+                              vaccine = NULL,
+                              yovi = imcover::get_yovi(),
+                              na.rm = FALSE){
+
+  if(!inherits(yovi, "data.frame")){
+    stop("Please provide valid 'yovi' data.")
+  }
+
+  if(any(!c('ISO3Code', 'vaccine', 'year_introduced') %in% names(yovi))){
+    stop("Non-matching column names in YOVI table.")
+  }
+
+  if(!is.null(vaccine)){
+    yovi <- yovi[yovi$vaccine %in% vaccine, ]
+  } else{
+    vaccine <- unique(yovi$vaccine)
+  }
+  if(nrow(yovi) < 1L) stop("No vaccine records found in YOVI table.")
+
+  # extract posterior data
+  dat <- X$posterior
+
+  dat <- merge(dat, yovi[, c("ISO3Code", "vaccine", "year_introduced")],
+               by.x = c("country", "vaccine"),
+               by.y = c("ISO3Code", "vaccine"),
+               suffixes = c("", ".yovi"),
+               all.x = TRUE,
+               sort = FALSE)
+  nm <- c("year_introduced.yovi", "year_introduced")
+  nm <- nm[which(nm %in% names(dat))]
+
+  # filter
+  if(na.rm){
+    del <- which((is.na(dat[[nm]]) & dat[["vaccine"]] %in% vaccine) | dat[["time"]] <= dat[[nm]])
+  } else{
+    del <- which(!is.na(dat[[nm]]) & dat[["time"]] <= dat[[nm]])
+  }
+  dat[[nm]] <- NULL
+
+  X[['posterior']] <- dat[-del, ]
+
+  return(X)
+}
+
+
--- R/utils.R
+++ R/utils.R
@@ -117,66 +117,6 @@ get_yovi <- function(vaccine = NULL){
 }
 
 
-#' Filter ic data records based on year of vaccine introduction
-#' Exclude ic data records for years when a vaccine had not been fully
-#' introduced in a country.
-#' @param X An object of class \code{ic.df}.
-#' @param vaccine Vector of vaccine codes as characters. Default is \code{NULL}
-#'   which includes all vaccines in object \code{X}.
-#' @param yovi Table of the year of vaccine introduction. See details. Default
-#'   value comes from \code{imcover::get_yovi()}.
-#' @param na.rm Should records which do not have any data for year of
-#'   introduction be removed? Default is \code{FALSE} to keep \code{NA} records
-#'   in \code{X}.
-#' @details The \code{yovi} argument can take a user-defined table of values.
-#'   This must be a \code{data.frame} object with column names 'ISO3code',
-#'   'vaccine' and 'year_introduced'.
-#' @seealso \code{\link[imcover]{get_yovi}}
-#' @name filter_yovi
-#' @export
-filter_yovi <- function(X,
-                        vaccine = NULL,
-                        yovi = imcover::get_yovi(),
-                        na.rm = FALSE){
-  if(!is.ic_data(X)){
-    stop("Please provide valid 'ic' data.")
-  }
-
-  if(!inherits(yovi, "data.frame")){
-    stop("Please provide valid 'yovi' data.")
-  }
-
-  if(any(!c('ISO3code', 'vaccine', 'year_introduced') %in% names(yovi))){
-    stop("Non-matching column names in YOVI table.")
-  }
-
-  if(!is.null(vaccine)){
-    yovi <- yovi[yovi$vaccine %in% vaccine, ]
-  } else{
-    vaccine <- unique(yovi$vaccine)
-  }
-  if(nrow(yovi) < 1L) stop("No vaccine records found in YOVI table.")
-
-  X <- merge(X, yovi[, c("ISO3code", "vaccine", "year_introduced")],
-             by.x = get_attr(X, attrs = c("country", "vaccine")),
-             by.y = c("ISO3code", "vaccine"),
-             suffixes = c("", ".yovi"),
-             all.x = TRUE, sort = FALSE)
-  nm <- c("year_introduced.yovi", "year_introduced")
-  nm <- nm[which(nm %in% names(X))]
-
-  # filter
-  if(na.rm){
-    del <- which((is.na(X[[nm]]) & X[[get_attr(X, "vaccine")]] %in% vaccine) | X[[get_attr(X, "time")]] <= X[[nm]])
-  } else{
-    del <- which(!is.na(X[[nm]]) & X[[get_attr(X, "time")]] <= X[[nm]])
-  }
-  X[[nm]] <- NULL
-
-  return(X[-del, ])
-}
-
-
 #' Get regional groupings for country codes
 #' Retrieve a list of the region based on the ISO3c alpha codes.
 #' @param country Vector of country ISO3 codes as characters. Default is \code{NULL}
