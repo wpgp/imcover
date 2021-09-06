b5d2d5a30962d8fb00670128dfe2fede9daf4a2b
Chris Jochem <wcjochem@users.noreply.github.com>
2021-09-06 11:29:29 +0100

    interface change - renaming fitting func

--- R/fit_stan.R
+++ R/fit_stan.R
@@ -2,26 +2,38 @@
 #' Multi-source immunisation coverage model with Stan
 #'
 #' @param X Object of \code{ic.df} for analysis
+#' @param region Logical. Should region-specific models be generated?. Default
+#'   is \code{TRUE}.
 #' @param verbose Logical. Should messages be displayed? Default is \code{TRUE}.
 #' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
 #' @return An object of class `stanfit` returned by `rstan::sampling`
 #'
-#' @name multi_lik_stan
+#' @name ic_fit
 #' @export
-multi_lik_stan <- function(X, verbose = TRUE, ...) {
-  if(!is.ic_data(X)) stop("Please provide valid 'ic' data.")
+ic_fit <- function(X, region = TRUE, verbose = TRUE, ...){
+  UseMethod("ic_fit")
+}
+
+
+#' @name ic_fit
+#' @export
+ic_fit.ic.df <- function(X, region = TRUE, verbose = TRUE, ...){
 
   # check data
   X <- X[!is.na(X[[get_attr(X, 'coverage')]]), ]
-  X <- X[!is.na(X[[get_attr(X, 'region')]]), ]
 
-  # get regions
-  regions <- unique(X[[get_attr(X, 'region')]])
-  regions <- regions[!is.na(regions)]
+  if(region){
+    X <- X[!is.na(X[[get_attr(X, 'region')]]), ]
 
-  if(length(regions) == 1){
+    # get regions
+    regions <- unique(X[[get_attr(X, 'region')]])
+    regions <- regions[!is.na(regions)]
+  }
+
+  # calculation
+  if(!region || length(regions) == 1){
     # calculate
-    out <- ic_fit(dat, verbose, ...)
+    out <- multi_lik_stan(dat, verbose, ...)
   } else{
     # main processing loop
     out <- lapply(regions, function(r){
@@ -29,73 +41,13 @@ multi_lik_stan <- function(X, verbose = TRUE, ...) {
       # subset
       dat <- X[X[[get_attr(X, 'region')]] == r, ]
       # calculate
-      fit <- ic_fit(dat, verbose, ...)
+      fit <- multi_lik_stan(dat, verbose, ...)
 
       return(fit)
     })
     names(out) <- regions
     class(out) <- list("iclist", class(out))
   }
-
-  return(out)
-}
-
-
-#' Prepare 'ic' data for Stan model
-#'
-#' @param X Object of \code{ic.df} to be converted
-#' @param make_index Logical. Should indices for e.g. 'country' be created?
-#' @return A list with named objects suitable for use as Stan data.
-#'   Default is \code{TRUE}.
-#' @keywords internal
-ic_to_stan <- function(X, make_index = TRUE){
-  stopifnot(is.ic_data(X))
-
-  # swap names
-  nn <- get_attr(X, ic_core())
-  X <- data.frame(X)
-  idx <- match(nn, names(X))
-  names(X)[na.omit(idx)] <- names(nn)[which(!is.na(idx))]
-
-  # sort
-  X <- X[order(X$source, X$country, X$vaccine, X$time), ]
-
-  # modify outcome
-  X$coverage <- X$coverage / 100
-  X$cov.logit <-  logit(X$coverage)  # log(X$coverage / (1 - X$coverage))
-
-  # create index values
-  X$v <- as.numeric(factor(X$vaccine))
-  X$i <- as.numeric(factor(X$country))
-  X$t <- as.numeric(factor(X$time)) # temporal trend per country
-  X$s <- as.numeric(factor(X$source))
-
-  vax_dat <- list(y = X$cov.logit,
-                  i = X$i,
-                  j = X$v,
-                  t = X$t,
-                  N = nrow(X),
-                  N_a = sum(tolower(X$source) == "admin"),
-                  N_o = sum(tolower(X$source) == "official"),
-                  N_s = sum(tolower(X$source) == "survey"),
-                  N_i = max(X$i),
-                  N_j = length(unique(X$vaccine)),
-                  N_t = max(X$t),
-                  start_o = min(which(tolower(X$source) == "official")),
-                  start_s = min(which(tolower(X$source) == "survey")))
-
-  return(vax_dat)
-}
-
-
-ic_fit <- function(X, verbose, ...){
-  # data prep
-  vax_data <- ic_to_stan(X, make_index = TRUE)
-
-  out <- rstan::sampling(stanmodels$multi_lik,
-                         data = vax_data,
-                         show_messages = verbose,
-                         ...)
 
   # calculate prediction ('mu')
   posterior <- t(as.data.frame(out, 'mu'))
@@ -143,5 +95,74 @@ ic_fit <- function(X, verbose, ...){
   out <- list('fit' = out, 'posterior' = posterior, 'data' = vax_data)
   class(out) <- list('icfit', class(out))
   return(out)
+}
+
+
+#' Multi-source immunisation coverage model with Stan
+#'
+#' @param X Object of \code{ic.df} for analysis
+#' @param verbose Logical. Should messages be displayed? Default is \code{TRUE}.
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
+#' @return An object of class `stanfit` returned by `rstan::sampling`
+#'
+#' @name multi_lik_stan
+#' @keywords internal
+multi_lik_stan <- function(X, verbose = TRUE, ...) {
+  if(!is.ic_data(X)) stop("Please provide valid 'ic' data.")
+
+  # data prep
+  vax_data <- ic_to_stan(X, make_index = TRUE)
+
+  # call stan model
+  out <- rstan::sampling(stanmodels$multi_lik,
+                         data = vax_data,
+                         show_messages = verbose,
+                         ...)
+  return(out)
+}
+
+
+#' Prepare 'ic' data for Stan model
+#'
+#' @param X Object of \code{ic.df} to be converted
+#' @param make_index Logical. Should indices for e.g. 'country' be created?
+#' @return A list with named objects suitable for use as Stan data.
+#'   Default is \code{TRUE}.
+#' @keywords internal
+ic_to_stan <- function(X, make_index = TRUE){
+  stopifnot(is.ic_data(X))
+
+  # swap names
+  X <- swap_names(X)
+  X <- data.frame(X)
+
+  # sort
+  X <- X[order(X$source, X$country, X$vaccine, X$time), ]
+
+  # modify outcome
+  X$coverage <- X$coverage / 100
+  X$cov.logit <-  logit(X$coverage)  # log(X$coverage / (1 - X$coverage))
+
+  # create index values
+  X$v <- as.numeric(factor(X$vaccine))
+  X$i <- as.numeric(factor(X$country))
+  X$t <- as.numeric(factor(X$time)) # temporal trend per country
+  X$s <- as.numeric(factor(X$source))
+
+  vax_dat <- list(y = X$cov.logit,
+                  i = X$i,
+                  j = X$v,
+                  t = X$t,
+                  N = nrow(X),
+                  N_a = sum(tolower(X$source) == "admin"),
+                  N_o = sum(tolower(X$source) == "official"),
+                  N_s = sum(tolower(X$source) == "survey"),
+                  N_i = max(X$i),
+                  N_j = length(unique(X$vaccine)),
+                  N_t = max(X$t),
+                  start_o = min(which(tolower(X$source) == "official")),
+                  start_s = min(which(tolower(X$source) == "survey")))
+
+  return(vax_dat)
 }
 
