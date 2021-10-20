69f41a020770337b7011ea12b8e845fa23bb5491
Chris Jochem <wcjochem@users.noreply.github.com>
2021-10-20 17:22:53 +0100

    drop old stan code

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,14 +12,10 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4multi_lik_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4multi_lik_v2_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4single_lik_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4multi_lik_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4multi_lik_mod, 0},
     {"_rcpp_module_boot_stan_fit4multi_lik_v2_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4multi_lik_v2_mod, 0},
-    {"_rcpp_module_boot_stan_fit4single_lik_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4single_lik_mod, 0},
     {NULL, NULL, 0}
 };
 
