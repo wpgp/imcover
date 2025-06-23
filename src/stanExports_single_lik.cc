3c69abb9dc385d16b38bb796ae8bfe1f1c27d329
Chris Jochem <wcjochem@users.noreply.github.com>
2025-06-23 07:30:39 +0100

    feat: update and rebuilding stan model definitions

--- src/stanExports_single_lik.cc
+++ src/stanExports_single_lik.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4single_lik_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_single_lik")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_single_lik")
 
     .constructor<SEXP,SEXP,SEXP>()
 
